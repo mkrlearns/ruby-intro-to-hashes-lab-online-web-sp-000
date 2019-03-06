7e36f34be5a2ae0808a146ac9c816457cd3c3d15
ryan m <contact@ryanmeek.com>
2019-03-06 23:21:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,12 @@ def monopoly_with_second_tier
     railroads: {
       pieces: 4,
       names: {},
-      rent_in_dollars: {}
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_piece_owned: 50,
+        three_piece_owned: 100,
+        four_piece_owned: 200
+      }
     }}
 end
 
